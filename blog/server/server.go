5e421cd69b7de0b27d3bd124f3978a2e201958f9
Vitor Castro <vitorhugocastro95@gmail.com>
2019-08-08 00:46:11 +0100

    Added Reflection to Blog server

--- blog/server/server.go
+++ blog/server/server.go
@@ -9,6 +9,8 @@ import (
 	"os/signal"
 	"time"
 
+	"google.golang.org/grpc/reflection"
+
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
 
@@ -239,6 +241,8 @@ func main() {
 	opts := []grpc.ServerOption{}
 	s := grpc.NewServer(opts...)
 	blogpb.RegisterBlogServiceServer(s, &server{})
+
+	reflection.Register(s)
 
 	go func() {
 		log.Println("Starting server...")
